543fdf00bc2815e021cb99f0848547a9809800b9
RAHULDAS6009 <das206053@gmail.com>
2025-02-04 21:15:23 +0530

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { InputfeildComponent } from './components/inputfeild/inputfeild.component';
-import { ButtonComponent } from './components/button/button.component';
-import { SelectComponent } from './components/select/select.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
+// import { InputfeildComponent } from './components/inputfeild/inputfeild.component';
+// import { ButtonComponent } from './components/button/button.component';
+// import { SelectComponent } from './components/select/select.component';
 import { WorkoutListComponent } from './pages/workout-list/workout-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [WorkoutListComponent],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UserFormComponent } from './pages/user-form/user-form.component';
+import { WorkoutListComponent } from './pages/workout-list/workout-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:UserFormComponent},
+    {path:"userlist",component:WorkoutListComponent},
+];
